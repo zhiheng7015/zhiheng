08c5bb5e6df893884c7b18a9b16db147d9268a2a
zhiheng7015 <1371235786@qq.com>
2019-05-21 11:48:44 +0800

    1

---
+++ src/main/java/com/zhiheng/community/controller/helloController.java
@@ -0,0 +1,15 @@
+package com.zhiheng.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class helloController {
+    @GetMapping("/")
+    public String hello(){
+
+    return "index";
+    }
+}
