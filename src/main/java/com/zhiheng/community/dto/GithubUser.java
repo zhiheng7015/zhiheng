5edd3b6e4d32808439dea9b1df4202becf68dd76
zhiheng7015 <1371235786@qq.com>
2019-05-29 11:53:25 +0800

    lombok

--- src/main/java/com/zhiheng/community/dto/GithubUser.java
+++ src/main/java/com/zhiheng/community/dto/GithubUser.java
@@ -8,36 +8,4 @@ public class GithubUser {
     private  long id;
     private  String bio;
     private String avatar_url;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public String getAvatar_url() {
-        return avatar_url;
-    }
-
-    public void setAvatar_url(String avatar_url) {
-        this.avatar_url = avatar_url;
-    }
 }
